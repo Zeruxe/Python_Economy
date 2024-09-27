59251da9c52a2b9846db41a56c6c2f2337c6e630
Yoseph Naoom <yosephsarab@gmail.com>
2024-09-27 14:16:17 +0200

    Fixed that 0.5 and above in decimals always rounds up

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import math
+
 def main():
-    x = 1.5  # Buy multiplier
-    y = 2    # Sell multiplier
+    x = 0.5  # Buy multiplier
+    y = 1.2  # Sell multiplier
     
     input_file = 'input.txt'
     output_file = 'output.txt'
@@ -22,13 +24,19 @@ def read_items_from_file(input_file):
             items[item_name] = {'buy': buy_price, 'sell': sell_price}
     return items
 
+def custom_round(value):
+    if value - int(value) >= 0.5:
+        return math.ceil(value)
+    else:
+        return int(value)
+
 def write_items_to_file(items, x_multiplier, y_multiplier, output_file):
     with open(output_file, 'w') as file:
         item_count = len(items)
         for index, (item_name, prices) in enumerate(items.items()):
             formatted_name = item_name.replace(' ', '_').upper()
-            new_buy = int(prices['buy'] * x_multiplier)
-            new_sell = int(prices['sell'] * y_multiplier)
+            new_buy = custom_round(prices['buy'] * x_multiplier)
+            new_sell = custom_round(prices['sell'] * y_multiplier)
             file.write(f"{formatted_name} {{\n")
             file.write(f"    sellingValue: {new_sell}\n")
             file.write(f"    buyValue: {new_buy}\n")
